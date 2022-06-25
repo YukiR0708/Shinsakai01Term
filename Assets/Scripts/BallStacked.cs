be8e4c8d7253898269d155c53e9521aba6556e45
Ray <rei.piyo.55687@gmail.com>
2022-06-25 18:03:14 +0900

    エラー修正

--- Assets/Scripts/BallStacked.cs
+++ Assets/Scripts/BallStacked.cs
@@ -9,21 +9,18 @@ using UnityEngine;
 
 public class BallStacked : MonoBehaviour
 {
-    [Header("ボールプレハブ(奇数）"), SerializeField] GameObject _ballObj1;
-    [Header("ボールプレハブ(偶数）"), SerializeField] GameObject _ballObj2;
-    [Header("リスポーンマネージャー"), SerializeField] GameObject _respawnManager;
+    GameObject _ballObj;
+    [SerializeField] GameObject _respawnManager;
 
     public void ButtonPushed()  //ボタンが押されたら
     {
-        if (_ballObj1 != null)
+        _ballObj = GameObject.FindGameObjectWithTag("Ball");
+
+        if (_ballObj != null)
         {
-            Destroy(_ballObj1);  //ボールを消す
+            Destroy(_ballObj);  //ボールを消す
         }
 
-        if (_ballObj2 != null)
-        {
-            Destroy(_ballObj2);  //ボールを消す
-        }
 
         _respawnManager.GetComponent<RespawnManager>().Respawn();   //リスポーンマネージャーのRespawnメソッドを呼ぶ
     }
